f7ca17d65b33cd2bd831fe5ec8264377ebe9d67a
thordossche <dossche.thor@gmail.com>
2022-04-02 10:06:27 +0200

    fix python indentation error

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -2,10 +2,11 @@ local configs = require'nvim-treesitter.configs'
 	configs.setup {
 	ensure_installed = "maintained", -- Only use parsers that are maintained
 	highlight = { -- enable highlighting
-		enable = true, 
+		enable = true,
 	},
 	indent = {
 		enable = true, -- default is disabled anyways
+		disable = { "python" },
 	},
 	playground = {
 		enable = true,
