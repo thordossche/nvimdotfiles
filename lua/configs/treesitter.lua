a2fde9225bcc814b9a721b7e42a2f15ee1931ab7
thordossche <dossche.thor@gmail.com>
2022-10-21 20:32:06 +0200

    update for treesitter

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,6 +1,5 @@
 local configs = require'nvim-treesitter.configs'
 	configs.setup {
-	ensure_installed = "maintained", -- Only use parsers that are maintained
 	highlight = { -- enable highlighting
 		enable = true,
 	},
