a89f69637dd93a337d70a12f365df96fb001ece2
thordossche <dossche.thor@gmail.com>
2022-10-21 20:30:09 +0200

    add lazygit console

--- lua/configs/toggleterm.lua
+++ lua/configs/toggleterm.lua
@@ -17,3 +17,12 @@ require("toggleterm").setup{
     }
   }
 }
+
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, })
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>G", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
