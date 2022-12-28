2ed8a94fceec1edd8d87954cf2be2f246862bb7d
thordossche <dossche.thor@gmail.com>
2022-12-28 16:40:50 +0100

    changes to telescipe

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -4,12 +4,11 @@ require('telescope').setup({})
 require('telescope').load_extension('fzf')
 require('telescope').load_extension('neoclip')
 
-vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope file_browser<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>Telescope find_files<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>tg', '<cmd>Telescope live_grep<cr>', { noremap = true })
 vim.api.nvim_set_keymap(
   'n',
-  '<leader>fh',
+  '<leader>th',
   '<cmd>lua require("telescope.builtin").current_buffer_fuzzy_find({sorting_strategy=ascending})<cr>',
   { noremap = true }
 )
