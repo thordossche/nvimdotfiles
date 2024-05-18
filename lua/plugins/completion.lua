a5e1ed36c4f1ab4080981672d21b424c6e54a095
Thor Dossche <thor@accurat.ai>
2024-05-18 21:45:18 +0200

    add more completion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -8,8 +8,9 @@ return {
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-buffer",
-            -- { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
-            -- "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-cmdline",
+            { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
+            "saadparwaiz1/cmp_luasnip",
         },
         config = function()
             vim.opt.completeopt = { "menu", "menuone", "noselect" }
@@ -19,10 +20,11 @@ return {
             lspkind.init({})
 
 
-            local cmp = require "cmp"
+            local cmp = require("cmp")
 
             cmp.setup {
                 sources = {
+                    { name = "luasnip" },
                     { name = "nvim_lsp" },
                     { name = "path" },
                     { name = "buffer" },
@@ -38,14 +40,11 @@ return {
                         { "i", "c" }
                     ),
                 },
-
-                -- Enable luasnip to handle snippet expansion for nvim-cmp
-                -- snippet = {
-                --     expand = function(args)
-                --         require("luasnip").lsp_expand(args.body)
-                --     end,
-                --     },
-
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
                 formatting = {
                     format = lspkind.cmp_format {
                         with_text = true,
@@ -61,35 +60,66 @@ return {
                 }
             }
 
-            -- Setup up vim-dadbod
-            -- cmp.setup.filetype({ "sql" }, {
-            --     sources = {
-            --         { name = "vim-dadbod-completion" },
-            --         { name = "buffer" },
-            --     },
-            -- })
+            -- Add brackets for function completion
+            local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+            cmp.event:on(
+              'confirm_done',
+              cmp_autopairs.on_confirm_done()
+            )
 
-            -- local ls = require "luasnip"
-            -- ls.config.set_config {
-            --     history = false,
-            --     updateevents = "TextChanged,TextChangedI",
-            -- }
+            -- Setup up vim-dadbod
+            cmp.setup.filetype({ "mysql" }, {
+                sources = {
+                    { name = "vim-dadbod-completion" },
+                    { name = "buffer" },
+                },
+            })
+
+            -- seach autocompletion 
+            cmp.setup.cmdline("/", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = "buffer" },
+                },
+            })
+
+            -- cmdline autocompletion
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                    sources = cmp.config.sources({
+                        { name = "path" },
+                    }, {
+                    {
+                    name = "cmdline",
+                        option = {
+                            ignore_cmds = { "Man", "!" },
+                        },
+                    },
+                }),
+            })
+
+            -- Setup luasnip
+            local ls = require "luasnip"
+            ls.config.set_config {
+                history = false,
+                updateevents = "TextChanged,TextChangedI",
+            }
 
             -- for _, ft_path in ipairs(vim.api.nvim_get_runtime_file("lua/custom/snippets/*.lua", true)) do
             --     loadfile(ft_path)()
             -- end
 
-            -- vim.keymap.set({ "i", "s" }, "<c-k>", function()
-            --     if ls.expand_or_jumpable() then
-            --         ls.expand_or_jump()
-            --     end
-            -- end, { silent = true })
+            vim.keymap.set({ "i", "s" }, "<c-k>", function()
+                if ls.expand_or_jumpable() then
+                    ls.expand_or_jump()
+                end
+            end, { silent = true })
 
-            -- vim.keymap.set({ "i", "s" }, "<c-j>", function()
-            --     if ls.jumpable(-1) then
-            --         ls.jump(-1)
-            --     end
-            -- end, { silent = true })
+            vim.keymap.set({ "i", "s" }, "<c-j>", function()
+                if ls.jumpable(-1) then
+                    ls.jump(-1)
+                end
+            end, { silent = true })
 
         end,
     },
