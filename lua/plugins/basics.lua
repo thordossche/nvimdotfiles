6339f2a0a5b0a9c98e087677217cd5b839d5b901
Thor Dossche <thor@accurat.ai>
2024-05-18 17:21:58 +0200

    add basic plugins

---
+++ lua/plugins/basics.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        "kylechui/nvim-surround",
+        event = "VeryLazy",
+        opts = {}
+    },
+    {
+        'numToStr/Comment.nvim',
+        lazy = false,
+        opts = {}
+    },
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        opts = {}
+    }
+}
