09009631c87736c668c074330e78d4e5595ae528
Thor Dossche <thor@accurat.ai>
2024-05-18 17:02:42 +0200

    add keymaps and settings

---
+++ plugin/settings.lua
@@ -0,0 +1,22 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+
+opt.scrolloff = 10
+opt.expandtab = true
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.splitbelow = true
+opt.splitright = true
+
+opt.mouse = 'a'
+opt.laststatus = 3
+
+-- Might want to add this later?
+-- vim.g.python_highlight_space_errors = 0
+-- let g:python_highlight_space_errors = 0
+
+
