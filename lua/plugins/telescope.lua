c00f9295943b0c9372492c10d7d6dfcf82807ec3
Thor Dossche <thor@accurat.ai>
2024-09-28 18:40:21 +0200

    telescope fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,10 @@ return {
     'nvim-telescope/telescope.nvim',
     dependencies = {
         'nvim-lua/plenary.nvim',
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+        {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
+        }
     },
     config = function()
         require('telescope').setup{
