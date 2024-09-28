92a932c6d32d1e2aa3b89292a9bdcb6e57e72a92
Thor Dossche <thor@accurat.ai>
2024-09-28 18:39:49 +0200

    switch leap for flash

--- lua/plugins/basics.lua
+++ lua/plugins/basics.lua
@@ -20,12 +20,16 @@ return {
         end
     },
     {
-        "ggandor/leap.nvim",
-        config = function()
-            local leap = require('leap')
-            leap.add_default_mappings()
-            leap.opts.case_sensitive = true
-        end,
+      "folke/flash.nvim",
+      event = "VeryLazy",
+      opts = {},
+      keys = {
+        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+        { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+        { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+        { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      },
     },
     {
         'metakirby5/codi.vim'
